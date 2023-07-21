813005408a9a13b8c86da1ccd7133937416b8a98
Hafeezdeveloper <hafeez.hussain2003@gmail.com>
2023-07-21 13:35:31 -0700

    pyclsecond

---
+++ abc.py
@@ -0,0 +1,113 @@
+# python follow snake case --->     abc_name
+
+# print("Awdawd")
+
+
+# !!!!!!!!!!!!!!---Conactination-----------
+# a = "hafez"
+# b = "hussain"
+
+
+# x = 4
+# x = 4
+# print(a+b)
+# print(a+b)
+
+# print("4"+ "3")
+# -------------String--------------------
+# name = "hafeez"
+# age = "hussain"
+
+#         # ===========Method  1 ============
+# print(f"My name is {name} and i am about {age}")
+
+
+#         # ===========Method  2 ============
+# print("My name is {} and i am about {}".format(name,age))
+
+# # ===========Method  3 ============
+# print("My name is %s and i am about %s" %(name,age))
+
+
+
+
+# ------------------Input function-------------------
+        # ===========Method 1========
+# print(input("enter your name"))
+
+#         # ===========Method 2========
+# age = input("enter your age")
+# print(age)    
+
+
+# x = input("enter your name \n")
+# print(x)
+
+# x = input("enter name")
+# # b = input(f"Hi {x}")
+# print(f"Hi {x} ! Welcome back")
+
+# x = input("num 1 : \t")
+# b = input("num 2 : \t")
+# print( int(x) + int(b) )
+
+# x=int(x)
+# b=int(b)
+# print( x + b )
+
+
+# x = input("num 1 : \t")
+
+# x= int(x)
+
+# print( x*x )
+
+# x = input("math 1 : \t")
+# b = input("urdu 1 : \t")
+
+# x= int(x)
+# b= int(b)
+
+# print( "The Sum is  " , x+b  )
+# print( "The muyiplication is  " , x*b  )
+# print( "The Divison  is  " , int(x/b)  )
+# print( "The Subtraction  is  " , x-b  )
+
+
+
+# x = 5 
+# y = 2
+
+# z1 = x
+# z2 = y
+
+
+# print("The value of x is",z2)
+# print("The value of y is",z1)
+
+
+
+# ==========bultin meythod=====
+
+# x = "my name is hafeez"
+# print(len(x))
+# print(x.replace("hafeez","hussain"))
+# let g = 1.45635
+# print(round(g,3))
+# print(isinstance(x, str))
+
+
+
+# ==============Modulus ======
+# reminder bata rha hota h 
+# x = 10
+# here we used for even odd
+
+# print(x % 2)
+
+
+
+
+
+
+
